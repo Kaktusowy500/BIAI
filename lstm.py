2d84826716e616819797aa79b6b3d98d32ce6122
Kaktusowy500 <k.lewand500@gmail.com>
2022-05-21 23:23:27 +0200

    Add basic training script, clean up

--- lstm.py
+++ lstm.py
@@ -2,19 +2,19 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable 
 
-class LSTM1(nn.Module):
+class CustomLSTM(nn.Module):
     def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
-        super(LSTM1, self).__init__()
-        self.num_classes = num_classes #number of classes
-        self.num_layers = num_layers #number of layers
-        self.input_size = input_size #input size
-        self.hidden_size = hidden_size #hidden state
-        self.seq_length = seq_length #sequence length
+        super(CustomLSTM, self).__init__()
+        self.num_classes = num_classes
+        self.num_layers = num_layers 
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.seq_length = seq_length
 
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                          num_layers=num_layers, batch_first=True) #lstm
-        self.fc_1 =  nn.Linear(hidden_size, 128) #fully connected 1
-        self.fc = nn.Linear(128, num_classes) #fully connected last layer
+                          num_layers=num_layers, batch_first=True) 
+        self.fc_1 =  nn.Linear(hidden_size, 128) 
+        self.fc = nn.Linear(128, num_classes) 
 
         self.relu = nn.ReLU()
     
@@ -25,8 +25,8 @@ class LSTM1(nn.Module):
         output, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
         hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
         out = self.relu(hn)
-        out = self.fc_1(out) #first Dense
-        out = self.relu(out) #relu
+        out = self.fc_1(out)
+        out = self.relu(out) 
         out = self.fc(out) #Final Output
         return out
  
