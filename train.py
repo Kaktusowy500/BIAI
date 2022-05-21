0316965751017a2584c0078d13e57b3b2987daea
Kaktusowy500 <k.lewand500@gmail.com>
2022-05-22 00:18:57 +0200

    Split into functions/classes

--- train.py
+++ train.py
@@ -2,64 +2,56 @@ from lstm import CustomLSTM
 import torch
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-import torch.nn as nn
-from torch.autograd import Variable
 import matplotlib.pyplot as plt
+from data_preparer import DataPreparer
+
+NUM_EPOCHS = 1000
+LEARNING_RATE = 0.001
+
+INPUT_SIZE = 5  # number of features
+HIDDEN_SIZE = 2  # number of features in hidden state
+NUM_LAYERS = 1  # number of stacked lstm layers
+
+NUM_CLASSES = 1  # number of output classes
+
+def plot_results(predicted_data, ground_truth):
+  data_predict = predicted_data.data.numpy()  # numpy conversion
+  dataY_plot = ground_truth.data.numpy()
+
+  data_predict = data_prep.mm_inverse_transform(data_predict)
+  dataY_plot =  data_prep.mm_inverse_transform(dataY_plot)
+
+  plt.figure(figsize=(10, 6))  # plotting
+  plt.axvline(x=200, c='r', linestyle='--')  # size of the training set
+
+  plt.plot(dataY_plot, label='Actuall Data')  # actual plot
+  plt.plot(data_predict, label='Predicted Data')  # predicted plot
+  plt.title('Time-Series Prediction')
+  plt.legend()
+  plt.show()
 
 
-df = pd.read_csv('SBUX.csv', index_col='Date', parse_dates=True)
+df = pd.read_csv("SBUX.csv", index_col='Date', parse_dates=True)
 
-# Prepare data
-X = df.iloc[:, :-1]  # params without volume
-y = df.iloc[:, 5:6]  # volume
+data_prep = DataPreparer()
+data_prep.split_and_convert_to_tensors(df)
 
-mm = MinMaxScaler()
-ss = StandardScaler()
-X_ss = ss.fit_transform(X)
-y_mm = mm.fit_transform(y)
+X_train_tensors_final = torch.reshape(data_prep.X_train_tensor, (data_prep.X_train_tensor.shape[0], 1, data_prep.X_train_tensor.shape[1]))
+X_test_tensors_final = torch.reshape(data_prep.X_test_tensor, (data_prep.X_test_tensor.shape[0], 1, data_prep.X_test_tensor.shape[1]))
 
+print("Training Shape", X_train_tensors_final.shape, data_prep.y_train_tensor.shape)
+print("Testing Shape", X_test_tensors_final.shape, data_prep.y_test_tensor.shape)
 
-X_train = X_ss[:200, :]
-X_test = X_ss[200:, :]
-
-y_train = y_mm[:200, :]
-y_test = y_mm[200:, :]
-
-X_train_tensors = Variable(torch.Tensor(X_train))
-X_test_tensors = Variable(torch.Tensor(X_test))
-
-y_train_tensors = Variable(torch.Tensor(y_train))
-y_test_tensors = Variable(torch.Tensor(y_test))
-
-
-X_train_tensors_final = torch.reshape(X_train_tensors, (X_train_tensors.shape[0], 1, X_train_tensors.shape[1]))
-
-
-X_test_tensors_final = torch.reshape(X_test_tensors, (X_test_tensors.shape[0], 1, X_test_tensors.shape[1]))
-
-print("Training Shape", X_train_tensors_final.shape, y_train_tensors.shape)
-print("Testing Shape", X_test_tensors_final.shape, y_test_tensors.shape)
-
-
-num_epochs = 1000
-learning_rate = 0.001
-
-input_size = 5  # number of features
-hidden_size = 2  # number of features in hidden state
-num_layers = 1  # number of stacked lstm layers
-
-num_classes = 1  # number of output classes
-lstm = CustomLSTM(num_classes, input_size, hidden_size, num_layers, X_train_tensors_final.shape[1])
+lstm = CustomLSTM(NUM_CLASSES, INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, X_train_tensors_final.shape[1])
 criterion = torch.nn.MSELoss()    # mean-squared error for regression
-optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(lstm.parameters(), lr=LEARNING_RATE)
 
-for epoch in range(num_epochs):
+for epoch in range(NUM_EPOCHS):
     outputs = lstm.forward(X_train_tensors_final)  # forward pass
     optimizer.zero_grad()  # caluclate the gradient, manually setting to 0
 
     # obtain the loss function
-    loss = criterion(outputs, y_train_tensors)
+    loss = criterion(outputs, data_prep.y_train_tensor)
 
     loss.backward()  # calculates the loss of the loss function
 
@@ -68,25 +60,9 @@ for epoch in range(num_epochs):
         print(f"Epoch: {epoch}, loss: {loss.item():.5f}")
 
 
-df_X_ss = ss.transform(df.iloc[:, :-1])  # old transformers
-df_y_mm = mm.transform(df.iloc[:, -1:])  # old transformers
-
-df_X_ss = Variable(torch.Tensor(df_X_ss))  # converting to Tensors
-df_y_mm = Variable(torch.Tensor(df_y_mm))
+df_X_ss, df_y_mm = data_prep.normalize_df_to_tensor(df)
 # reshaping the dataset
 df_X_ss = torch.reshape(df_X_ss, (df_X_ss.shape[0], 1, df_X_ss.shape[1]))
 
 train_predict = lstm(df_X_ss)  # forward pass
-data_predict = train_predict.data.numpy()  # numpy conversion
-dataY_plot = df_y_mm.data.numpy()
-
-data_predict = mm.inverse_transform(data_predict)  # reverse transformation
-dataY_plot = mm.inverse_transform(dataY_plot)
-plt.figure(figsize=(10, 6))  # plotting
-plt.axvline(x=200, c='r', linestyle='--')  # size of the training set
-
-plt.plot(dataY_plot, label='Actuall Data')  # actual plot
-plt.plot(data_predict, label='Predicted Data')  # predicted plot
-plt.title('Time-Series Prediction')
-plt.legend()
-plt.show()
+plot_results(train_predict, df_y_mm)
