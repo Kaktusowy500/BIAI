79d3beb1a270b67a16e30f3565d2f70655741e40
RocketFan <kubal123.tomczak@gmail.com>
2022-05-27 17:20:01 +0200

    Transfer code from jupyter notebook to python scripts

---
+++ download_data.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import pandas as pd
+import os
+
+try:
+    os.mkdir('raw_data')
+
+    symbols_df = pd.read_csv('bats_symbols.csv')
+    tickers = symbols_df['Name'].to_list()
+    tickers_str = ' '.join(tickers)
+
+    for ticker in tickers:
+        yf_df = yf.download(ticker, start='2021-01-01', end="2021-12-30")
+        yf_df.to_csv('raw_data/' + ticker + '.csv')
+
+except FileExistsError:
+    pass
