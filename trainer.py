080e3758451a7b1613e352b1cb6e407cd38fdcd2
Kaktusowy500 <k.lewand500@gmail.com>
2022-05-28 16:44:12 +0200

    Add trainer class

--- trainer.py
+++ trainer.py
@@ -9,13 +9,14 @@ import matplotlib.pyplot as plt
 from data_prep import DataPrep
 from lstm import LSTM
 
-NUM_EPOCHS = 100
-LEARNING_RATE = 0.001
+NUM_EPOCHS = 400
+LEARNING_RATE = 0.002
 HIDDEN_SIZE = 50  # number of features in hidden state
-
 
 TRAIN_PERIODS = 20  # number of days used for training and infering
 TEST_PERIODS = 5  # number of days used for test and predict
+
+DF_PATH = "data/YPS.csv"
 
 
 def plot_results(df, predictions):
@@ -36,37 +37,48 @@ def plot_results(df, predictions):
             "r-",
             label='Predicted Values')
     plt.legend()
-    plt.savefig('plot', dpi=600)
+    plt.savefig('plot ', dpi=600)
+
+
+class Trainer():
+    def __init__(self):
+        self.model = LSTM(input_size=1, hidden_size=HIDDEN_SIZE, output_size=TEST_PERIODS)
+        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=LEARNING_RATE)
+
+    def train(self, x_train, y_train):
+        self.model.train()
+        for epoch in range(NUM_EPOCHS):
+            for x, y in zip(x_train, y_train):
+                y_hat, _ = self.model(x)
+                self.optimizer.zero_grad()
+                loss = self.criterion(y_hat, y)
+                loss.backward()
+                self.optimizer.step()
+
+            if epoch % 20 == 0:
+                print(f'epoch: {epoch:4} loss:{loss.item():10.8f}')
+
+    def predict(self, data_period):
+        self.model.eval()
+        with torch.no_grad():
+            predictions, _ = self.model(data_period)
+        return predictions
 
 
 if __name__ == "__main__":
 
     data_prep = DataPrep()
-    data_prep.read_and_parse_df("data/YPS.csv")
+    data_prep.read_and_parse_df(DF_PATH)
     train, test = data_prep.train_test(TEST_PERIODS)
     train_scaled = data_prep.scale_data(train)
     x_train, y_train = data_prep.get_x_y_pairs(train_scaled, TRAIN_PERIODS, TEST_PERIODS)
 
-    model = LSTM(input_size=1, hidden_size=HIDDEN_SIZE, output_size=TEST_PERIODS)
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    trainer= Trainer()
+    trainer.train(x_train, y_train)
+    inference_period = train_scaled[-TRAIN_PERIODS:]
+    predictions = trainer.predict(inference_period)
 
-    model.train()
-    for epoch in range(NUM_EPOCHS):
-        for x, y in zip(x_train, y_train):
-            y_hat, _ = model(x)
-            optimizer.zero_grad()
-            loss = criterion(y_hat, y)
-            loss.backward()
-            optimizer.step()
-
-        if epoch % 20 == 0:
-            print(f'epoch: {epoch:4} loss:{loss.item():10.8f}')
-
-    model.eval()
-    with torch.no_grad():
-        predictions, _ = model(train_scaled[-TRAIN_PERIODS:])
     # Apply inverse transform to undo scaling
     predictions = data_prep.scaler.inverse_transform(np.array(predictions.reshape(-1, 1)))
-
     plot_results(data_prep.df, predictions)
